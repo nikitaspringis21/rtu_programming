01cac1456fecb7898fbe7f6539c86fd8c244c146
nikitaspringis <102158983+nikitaspringis21@users.noreply.github.com>
2023-02-09 18:27:52 +0200

    init
    06.02.2023

---
+++ about.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printInfo(){
+    cout << "+--------------------------------------------------------------+" << "\n";
+    cout << "|   Daugavpils Programming School            ___  ___  ___     |" << "\n";
+    cout << "|   Author: Nikita Springis                 |   \\| _ \\/__|     |" << "\n";
+    cout << "|   Group: C1                               | |) |  _/\\_ \\     |" << "\n";
+    cout << "|   Program: About                          |___/|_|  |__/     |" << "\n";
+    cout << "|   Date: 06.02.2023                                           |" << "\n";
+    cout << "+--------------------------------------------------------------+" << endl;
+}
+int main()
+{
+    printInfo();
+    return 0;
+}
